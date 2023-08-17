028453780735d9c20e9a315d15f1e568a43c01c7
Jacob Timmis <jacobtimmis07@gmail.com>
2023-08-17 19:26:26 +0100

    Update projects display

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-// Function to fetch and process the projects from projects.json
 function fetchProjects() {
-  fetch('projects.json')
+    fetch('projects.json')
     .then((response) => response.json())
     .then((data) => {
-      // Once the data is fetched, call the function to add projects to the list
-      addProjectsToList(data);
+        PopulateProjects(data)
     })
     .catch((error) => {
-      console.error('Error fetching projects:', error);
-    });
+        console.error('Error fetching projects:', error)
+    })
 }
 
-// Function to add projects to the list
-function addProjectsToList(projects) {
-  const projectList = document.getElementById('project-list');
-
-  // Loop through the projects and create list items for each project
-  projects.forEach((project) => {
-    const listItem = document.createElement('li');
-    listItem.textContent = project.name;
-    projectList.appendChild(listItem);
-  });
+function PopulateProjects(projects) {
+    const ProjList = document.getElementById('project-list')
+    projects.forEach((project) => {
+        let ProjRoot = AddElement(ProjList, 'div', '', 'project')
+        AddElement(ProjRoot, 'h3', project.name, 'project-name')
+        AddElement(ProjRoot, 'p', project.brief, 'project-brief')
+        AddElement(ProjRoot, 'a', 'Check it out here!', 'project-link').href = project.link
+        AddElement(ProjRoot, 'p', new Date(project.date).toDateString(), 'project-date')
+    })
 }
 
-// Call the fetchProjects function when the page loads
-document.addEventListener('DOMContentLoaded', fetchProjects);
+function AddElement(parent, type, textContent, classes) {
+    const element = document.createElement(type)
+    element.textContent = textContent
+    element.classList = classes
+    parent.appendChild(element)
+    return element
+}
+
+// Call fetchProjects when the page loads
+document.addEventListener('DOMContentLoaded', fetchProjects)
