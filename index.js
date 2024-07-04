9330106e1fc102158ce636f885458d8f71f46cc1
Jacob Timmis <jacobtimmis07@gmail.com>
2024-07-04 21:55:37 +0100

    Add icons and update hidden flags

--- index.js
+++ index.js
@@ -12,28 +12,35 @@ function fetchProjects() {
 function PopulateProjects(projects) {
     const ProjList = document.getElementById('project-list')
     projects.forEach((section) => {
+        if (section.hidden) { return }
         let Section = AddElement(ProjList, 'div', '', 'section')
         let SectionTitle = AddElement(Section, 'div', '', 'section-header')
         AddElement(SectionTitle, 'h2', section.title, 'section-title')
         AddElement(SectionTitle, 'p', section.brief, 'section-brief')
         section.list.forEach((project) => {
-            if (!project.hidden) {
-                let ProjRoot = AddElement(Section, 'div', '', 'project')
-                let ProjTitle = AddElement(ProjRoot, 'div', '', 'project-title')
-                AddElement(ProjTitle, 'h3', project.name, 'project-name')
-                let projDate = new Date(project.date)
-                if (!isNaN(projDate)) {
-                    let loc_date = projDate.toLocaleDateString('en', {
-                        day: 'numeric',
-                        year: 'numeric',
-                        month: 'long',
-                    })
-                    AddElement(ProjTitle, 'p', loc_date, 'project-date')
-                }
-                AddElement(ProjRoot, 'p', project.brief, 'project-brief')
-                if (project.link) {
-                    AddElement(ProjRoot, 'a', 'Check it out here!', 'project-link').href = project.link
-                }
+            if (project.hidden) { return }
+            let ProjRoot = AddElement(Section, 'div', '', 'project')
+            if (project.image) {
+                let img = document.createElement("img")
+                img.src = "icons/" + project.image
+                img.classList = 'project-icon'
+                ProjRoot.appendChild(img)
+            }
+            let ProjInfo = AddElement(ProjRoot, 'div', '', 'project-info')
+            let ProjTitle = AddElement(ProjInfo, 'div', '', 'project-title')
+            AddElement(ProjTitle, 'h3', project.name, 'project-name')
+            let projDate = new Date(project.date)
+            if (!isNaN(projDate)) {
+                let loc_date = projDate.toLocaleDateString('en', {
+                    day: 'numeric',
+                    year: 'numeric',
+                    month: 'long',
+                })
+                AddElement(ProjTitle, 'p', loc_date, 'project-date')
+            }
+            AddElement(ProjInfo, 'p', project.brief, 'project-brief')
+            if (project.link) {
+                AddElement(ProjInfo, 'a', 'Check it out here!', 'project-link').href = project.link
             }
         })
     })
