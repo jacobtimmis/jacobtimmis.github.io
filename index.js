3f3e3cded23c7f783daad4b3b57d5bf1f7adf901
Jacob Timmis <jacobtimmis07@gmail.com>
2025-05-09 11:48:02 +0100

    Fix where link is added

--- index.js
+++ index.js
@@ -19,29 +19,41 @@ function populateProjects(projects) {
         if (p.gif) {
             const projectEntry = starEntry.content.cloneNode(true)
             setupEntry(projectEntry)
-            const projectScreen = projectEntry.querySelector('.screen');
-            projectScreen.src = "gifs/" + p.gif            
+            const projectScreen = projectEntry.querySelector('.screen')
+            projectScreen.src = "gifs/" + p.gif
+            const entry = projectEntry.querySelector('.star-entry')
+            setupLink(entry)
             starGrid.appendChild(projectEntry)
         } else {
             const projectEntry = smallEntry.content.cloneNode(true)
             setupEntry(projectEntry)
+            const projRoot = projectEntry.querySelector('.project')
+            setupLink(projRoot)
             projList.appendChild(projectEntry)
         }
     }
 }
 
+function setupLink (node) {
+    let link = p.link
+    if (link) {
+        node.onclick = () => window.location.href = link
+        node.classList.add('clickable-project')
+    }
+}
+
 function setupEntry (projectEntry) {
-    const projectIcon = projectEntry.querySelector('.project-icon');
+    const projectIcon = projectEntry.querySelector('.project-icon')
     projectIcon.src = "icons/" + p.icon
-    const projectName = projectEntry.querySelector('.project-name');
+    const projectName = projectEntry.querySelector('.project-name')
     projectName.textContent = p.name
-    const projectRecommend = projectEntry.querySelector('.project-recommend');
+    const projectRecommend = projectEntry.querySelector('.project-recommend')
     if (p.recommended) {
         projectRecommend.style.display = 'block'
     } else {
         projectRecommend.style.display = 'none'
     }
-    const projectDate = projectEntry.querySelector('.project-date');
+    const projectDate = projectEntry.querySelector('.project-date')
     const date = new Date(p.date)
     if (!isNaN(date)) {
         const localeDate = date.toLocaleDateString('en', {
@@ -53,14 +65,8 @@ function setupEntry (projectEntry) {
     } else {
         projectDate.textContent = ""
     }
-    const projectBrief = projectEntry.querySelector('.project-brief');
+    const projectBrief = projectEntry.querySelector('.project-brief')
     projectBrief.textContent = p.brief
-    let link = p.link
-    if (link) {
-        const projRoot = projectEntry.querySelector('.project');
-        projRoot.onclick = () => window.location.href = link
-        projRoot.classList.add('clickable-project')
-    }
 }
 
 function addElement(parent, type, classes, textContent) {
