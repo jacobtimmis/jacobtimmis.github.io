524b586ce7c489c12d248c113a3fa2a43b0f2789
Jacob Timmis <jacobtimmis07@gmail.com>
2025-12-24 12:47:41 +0000

    Format add_project.py and add bool hack

--- add_project.py
+++ add_project.py
@@ -1,6 +1,9 @@
 import argparse
 import json
 
+HIDDEN_ARG = "hidden"
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--id", required=True, type=str)
@@ -8,9 +11,12 @@ def main():
     parser.add_argument("--brief", required=True, type=str)
     parser.add_argument("--date", required=True, type=str)
     parser.add_argument("--link", required=True, type=str)
-    parser.add_argument("--hidden", default=False, type=bool)
-    
+    parser.add_argument(f"--{HIDDEN_ARG}", default="False")
+
     args = vars(parser.parse_args())
+
+    if args[HIDDEN_ARG].lower() == "false":
+        args[HIDDEN_ARG] = False
 
     with open("projects.json", "r") as json_file:
         json_obj = json.loads(json_file.read())
@@ -20,7 +26,6 @@ def main():
     with open("projects.json", "w") as json_file:
         json.dump(json_obj, json_file, indent=4)
         json_file.write("\n")
-
 
 
 if __name__ == "__main__":
